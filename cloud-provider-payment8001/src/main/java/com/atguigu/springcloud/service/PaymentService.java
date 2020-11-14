1a978534b3541a074428f48de0308bee53c7356f
Rainbowlau <h57251180@gmail.com>
2020-11-14 19:10:03 +0800

    初始化

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud.service;
+
+import com.atguigu.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * Copyright (C)
+ * Author:   mino
+ * Date:     14/11/20 5:49 下午
+ * History:
+ */
+public interface PaymentService {
+    public int create (Payment payment ); // 写
+    public Payment getPaymentById ( @Param( "id" ) Long id );// 读取
+}
